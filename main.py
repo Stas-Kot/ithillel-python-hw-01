867a60d3a71c54eb2a3780ebb5f4eb831c072005
Stas Kotelnykov <staskotelnykov@gmail.com>
2023-03-15 21:20:22 +0200

    hw done

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ try:
     c = int(input('Insert integer number c: '))
 
     d = b * b - 4 * a * c
-    print(d)
-    print(d ** (0.5))
 
     if d == 0:
         x = -1 * b / (2 * a)
@@ -28,6 +26,9 @@ try:
         x1 = (-1 * b + d ** (0.5)) / (2 * a)
         x2 = (-1 * b - d ** (0.5)) / (2 * a)
         result = 'x1 = ' + str(x1) + ' and ' + 'x2 = ' + str(x2)
+
+    else:
+        result = 'Discriminant is less than zero and there are no solutions!'
 
     print(result)
 
--- test.py
+++ test.py
@@ -1,1 +1,4 @@
 print('Hello World!')
+import random
+d = random.random()
+print(abs(d) > 1)
